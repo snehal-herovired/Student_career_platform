be687a220255a42b3b831a92f8d70e1aae48a2db
Snehal <snehal9140@gmail.com>
2023-08-11 14:02:11 +0530

    connnection url changes

--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create a custom Axios instance with default headers
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your API's base URL
+  baseURL: 'https://viredstoreapi.prashantdey.in', // Replace with your API's base URL
 });
 
 // Add a request interceptor to dynamically set the token if available
