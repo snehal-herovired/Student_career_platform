66abc1d0a30374843121dc391aa1e66a0fe95ab5
pegais <snehal9140@gmail.com>
2023-08-24 16:09:43 +0530

    changes in maincontroller

--- frontend/src/components/Pages/controlPanel/MainControlPanel.js
+++ frontend/src/components/Pages/controlPanel/MainControlPanel.js
@@ -8,76 +8,60 @@ export default function MainControlPanel() {
     let [skillName, setSkillName] = useState([]);
     const [isEditingSkill, setIsEditingSkill] = useState(false);
 
-    
+
     useEffect(() => {
-        
+
         const fetchData = async () => {
             let response = await axiosInstance.get('/student/students');
-            console.log(resumeSkillData, "FROM MainControlPanel");
+            // console.log(resumeSkillData, "FROM MainControlPanel");
             setResumeSkillData(response.data)
-          
+
             // fetchDataResume();
         }
         fetchData()
-        
+
     }, [])
-    
-    const fetchDataResume = async () => {
-        let response = await axiosInstance.get(`/resume/${studentId}`);
-        console.log(response.data, "FROM USEEFFECT");
-        setSkillName(response.data || []);
+
+    const fetchDataResume = async (id) => {
+       try {
+        console.log(id, "STUDENTID SELECTED");
+        let response = await axiosInstance.get(`/resume/${id}`);
+        if(response.status !== 404 || response.status !== 500){
+          
+            console.log(response.data,"RESUME FOR THIS ID");
+            console.log(skillName, "skills");
+            setSkillName(response.data.skills);
+        }
+       } catch (error) {
+        console.log(error.message);
+        alert('Resume not found')
+        return;
+       }
 
 
     }
     async function handleChange(e) {
+       try {
         const id = e.target.name;
         const value = e.target.value;
         const response = await axiosInstance.get(`/student/students/${value}`)
- setStudentId(value)
+        setStudentId(value)
         if (!response.data.studentData) {
             alert('Invalid ID!')
             return;
         }
-        console.log(singleStudentData, "FROM handlechange Func..");
-        setSingleStudent(response.data)
-        fetchDataResume()
+        // console.log(singleStudentData, "FROM handlechange Func..");
+     setSingleStudent(response.data)
+       fetchDataResume(value)
+       } catch (error) {
+        console.log(error.message);
+       }
     }
 
-    const handleSkillChange = (index, field, value) => {
-        setSkillName((prevSkill) => {
-          const updatedSkill = [...prevSkill];
-          updatedSkill[index] = {
-            ...updatedSkill[index],
-            [field]: value,
-          };
-          return updatedSkill;
-        });
-      };
-    
-      const handleAddSkill = () => {
-        setSkillName((prevSkill) => [
-          ...prevSkill,
-          {
-            name: '',
-            proficiency: 0,
-          },
-        ]);
-      };
-    
-      const handleDeleteSkill = (index) => {
-        setSkillName((prevSkill) => {
-          const updatedSkill = [...prevSkill];
-          updatedSkill.splice(index, 1);
-          return updatedSkill;
-        });
-      };
-    
-      const saveSkillChanges =async () => {
-        setIsEditingSkill(false);
-        const updatedSkill = { ...skillName, skills: skillName };
-          let response = await axiosInstance.post(`/resume/create`, updatedSkill);
-          console.log(response.data);
-      };
+
+
+
+
     return (
         <>
             <div className="container" style={{ padding: '10px' }}>
@@ -143,83 +127,7 @@ export default function MainControlPanel() {
                             <div class="input-group mb-3">
                                 <input type="text" class="form-control" placeholder="skillname" aria-label="skillname" aria-describedby="basic-addon1" />
                             </div>
-                            <section className="about" style={{ marginTop: "3px" }}>
-                                <div className="container" data-aos="fade-up">
 
-                                    <div className="section-title" style={{ display: 'flex', flexDirection: 'column', alignItems: "center", marginTop: "2px" }}>
-                                        <br />
-                                        <h4 style={{ fontWeight: "bold", position: 'relative' }}>
-                                            Skill Information
-                                            <span
-                                                style={{ position: 'absolute', top: '-14px', fontSize: '30px', cursor: 'pointer', color: 'rgb(128,128,128,0.6)', marginLeft: '3px' }}
-                                                onClick={() => setIsEditingSkill(!isEditingSkill)}
-                                            >
-                                                <FaEdit />
-                                            </span>
-
-                                        </h4>
-                                    </div>
-
-                                    <div className="row" style={{ background: "" }}>
-
-                                        <div className="col-lg-8 pt-4 pt-lg-0 content" style={{ padding: "2px" }}>
-                                            <h3>Skill information</h3>
-
-                                            <div className="row" >
-                                                {isEditingSkill ? (
-                                                    <div  >
-                                                        {skillName?.map((edu, index) => (
-                                                            <div key={index}>
-
-                                                                <div class="input-group flex-nowrap">
-                                                                    <input type="text" class="form-control" placeholder="name" aria-label="name" aria-describedby="addon-wrapping"
-                                                                        value={edu.name}
-                                                                        style={{ marginRight: '3px' }}
-                                                                        onChange={(e) =>
-                                                                            handleSkillChange(index, "name", e.target.value)
-                                                                        }
-                                                                    />
-                                                                </div>
-
-                                                                <div class="input-group mb-3">
-                                                                    <input type="number" class="form-control" placeholder="proficiency" aria-label="proficiency" value={edu.proficiency}
-                                                                        style={{ marginRight: '3px' }}
-                                                                        onChange={(e) =>
-                                                                            handleSkillChange(index, "proficiency", e.target.value)
-                                                                        } />
-                                                                 
-                                                                </div>
-
-                                                                <button onClick={() => handleDeleteSkill(index)} style={{ margin: '3px' }} type='button' className='btn btn-success'>Delete</button>
-                                                            </div>
-                                                        ))}
-                                                        <button onClick={handleAddSkill} style={{ margin: '3px' }} type='button' className='btn btn-success'>Add Skill</button>
-                                                        {isEditingSkill && <button onClick={saveSkillChanges} type='button' className='btn btn-success'>Save</button>}
-
-                                                    </div>
-                                                ) : (
-                                                    <div  >
-                                                        <ul>
-                                                            {skillName?.skills?.map((edu, index) => (
-                                                                <li key={index}>
-                                                                    {edu.name} - {edu.proficiency}
-                                                                </li>
-                                                            ))}
-                                                        </ul>
-                                                    </div>
-                                                )}
-
-
-
-
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <br />
-                                    <br />
-                                </div>
-                            </section>
                         </div>
 
 
--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create a custom Axios instance with default headers
 const axiosInstance = axios.create({
-  baseURL: 'https://viredstoreapi.prashantdey.in', // Replace with your API's base URL
+  baseURL: 'http://localhost:5000', // Replace with your API's base URL
 });
 
 // Add a request interceptor to dynamically set the token if available
@@ -22,5 +22,5 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-const Url ='https://viredstoreapi.prashantdey.in'
+const Url ='http://localhost:5000'
 export { axiosInstance, Url }
